e118ba08e06c0d3c20f7e8b32430fc2c0f13a6b1
dgtyPedro <pedrogcs0104@gmail.com>
2024-01-11 18:08:50 -0300

    feat: adiciona estrutura inicial

---
+++ src/common/components/Navbar/Navbar.component.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-item nav-title">Logo aqui</div>
+      <div className="nav-item nav-group">
+        <button>Sobre</button>
+        <button>Fazer Login com Spotify</button>
+      </div>
+    </nav>
+  );
+};
